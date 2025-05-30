e4d7b6365bc6757d99fcb8c75320d264508542a9
Daniel van Ginneken <dcjvanginneken@gmail.com>
2025-05-30 15:18:30 +0200

    Bump version to 0.1.1 in plugin.php

--- plugin.php
+++ plugin.php
@@ -1,7 +1,7 @@
 <?php
 return array(
   'id'          => 'osticket:autocloser',
-  'version'     => '0.1.0',
+  'version'     => '0.1.1',
   'name'        => 'Auto Closer',
   'author'      => 'Daniël van Ginneken',
   'description' => 'Automatically transitions tickets from "Resolved" to "Closed" after a configurable waiting period (default: 7 days), keeping your support queue clean and efficient.',
